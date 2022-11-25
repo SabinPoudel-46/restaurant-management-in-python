238bcf16a8f31b688b331d4b759837daccb0d063
SabinPoudel-46 <91828300+SabinPoudel-46@users.noreply.github.com>
2022-11-25 14:10:44 +0530

    Update Resturant.py
    Changes done on displaying menu items, file truncate after the execution of program and minor improvements

--- Resturant.py
+++ Resturant.py
@@ -4,50 +4,73 @@ def menu():
     global bill_amount,name
     try:
         while True:
-            choice_menu=int(input("1.Nepali Cuisine 2.Indian Cuisine 3.Exit:"))
+            print("")
+            choice_menu=int(input("1.Nepali Cuisine 2.Indian Cuisine 3.Back: "))
             if choice_menu==1:
-                print('''1.Dal Bhat-->200\n2.Momo-->90\n3.Gundruk,Dhido-->400\n4.Sel Roti-->40''')
-                choice_nepali=int(input())
+                print("")
+                print('''
+                         1.DalBhat       Rs200
+                         2.Momos         Rs90
+                         3.Gundruk&Dhido Rs400
+                         4.SelRoti       Rs40
+                         5.Thakali Khana Rs550''')
+                choice_nepali=int(input("Enter Your Choice: "))
                 if choice_nepali==1:
                     bill_amount=bill_amount+200
                     f=open("Billamount.txt","a")
                     f.write("Dal Bhat-->Rs200\n")
                     f.close()
+                    print("DalBhat Added to Your Order")
                 elif choice_nepali==2:
                     bill_amount=bill_amount+90
                     f=open("Billamount.txt","a")
                     f.write("Momo-->Rs90\n")
                     f.close()
+                    print("Momos Added to Your Order")
                 elif choice_nepali==3:
                     bill_amount=bill_amount+400
                     f=open("Billamount.txt","a")
-                    f.write("Ghundruk,Dhido-->Rs400\n")
+                    f.write("Ghundruk&Dhido-->Rs400\n")
                     f.close()
+                    print("Ghundruk Added to Your Order")
                 elif choice_nepali==4:
                     bill_amount=bill_amount+40
                     f=open("Billamount.txt","a")
-                    f.write("Sel Roti-->40\n")
+                    f.write("SelRoti-->40\n")
                     f.close()
+                    print("SelRoti Added to Your Order")
+                elif choice_nepali==5:
+                    bill_amount=bill_amount+550
+                    f=open("Billamount.txt","a")
+                    f.write("Thankali Khana-->Rs550\n")
+                    f.close()
+                    print("ThakaliKhana Added to Your Order")
                 else:
                     print("Enter the valid Dish")
             elif choice_menu==2:
-                print('''1.Palak Paneer-->Rs170\n2.Chana Masala-->Rs60\n3.Fish Curry-->Rs500''')
-                choice_indian=int(input())
+                print('''
+                         1.Palak Paneer       Rs170
+                         2.Chana Masala       Rs60
+                         3.Fish Curry         Rs500''')
+                choice_indian=int(input("Enter Your Choice: "))
                 if choice_indian==1:
                     bill_amount=bill_amount+170
                     f=open("Billamount.txt","a")
                     f.write("Palak Paneer-->Rs170\n")
                     f.close()
+                    print("Palak Paneer Added to Your Order")
                 elif choice_indian==2:
                     bill_amount=bill_amount+60
                     f=open("Billamount.txt","a")
                     f.write("Chana Masala-->Rs60\n")
                     f.close()
+                    print("Chana Masala Added to Your Order")
                 elif choice_indian==3:
                     bill_amount=bill_amount+500
                     f=open("Billamount.txt","a")
                     f.write("Fish Curry-->Rs500\n")
                     f.close()
+                    print("Fish Curry Added to Your Order")
                 else:
                     print("Enter Valid choice")  
             elif choice_menu==3:
@@ -55,13 +78,14 @@ def menu():
     except:
         print("An Error Occured")
 def bill():
-    global name
+    global name,count
+    count=1
     bill_no=random.randint(1,10)
     bill_no=str(bill_no)
     bill_no="MER"+bill_no
     global bill_amount
     f=open("Billamount.txt","a")
-    f.write("""UniqueID for this order is {}\n{}'s final Bill amount is {}\nThanks for using our service""".format(bill_no,name,bill_amount))
+    f.write("""UniqueID for this order is {}\n{}'s final Bill amount is {}\nThanks for using our service """.format(bill_no,name,bill_amount))
     f.close()
     print("Your bill is ready to print")
 def display():
@@ -78,6 +102,8 @@ def pdf():
         PDF.cell(200,10,txt=i,ln=3,align='C')
     PDF.output("Bill.pdf")
     print("Pdf file Successfully Generated")
+    global count_pdf
+    count_pdf=1
 def search():
     bill_no=int(input("Enter the bill number: "))
     f=open("Billamount.txt","r")
@@ -90,14 +116,18 @@ def search():
 
 #-----RESTURANT Billing SYSTEM--------#
 
-bill_amount=0
+bill_amount,count,count_pdf=0,0,0
 project="Restaurant Billing System"
-print(project.center(40,"*"))
+print("")
+print(project.center(94,"*"))
+print("")
 name=input("Enter Your Name: ")
 try:
     while True:
+        print("")
         print("1.Menu 2.Generate Bill 3.Search Bill 4.Display Bill 5.Generate Pdf file 6.Exit")
-        choice=int(input("Enter Your Choice: "))
+        print("")
+        choice=int(input("Enter Your Choice {}: ".format(name)))
         if choice==1:
             menu()
         elif choice==2:
@@ -109,9 +139,20 @@ try:
         elif choice==5:
             pdf()
         elif choice==6:
-            print("Thanks for using our Service")
-            break
+            print("")
+            if count==0 :
+                print("{} Please Proceed Towards Billing".format(name))
+            elif count_pdf==0:
+                print("{} Please Proceed towards Pdf Genetation".format(name))
+            else:
+                print("Have a Good Day {}".format(name))
+                f = open('Billamount.txt', 'r+')
+                f.truncate(0)
+                break
 except:
+    print("")
     print("An Error Occured")
 finally:
-    print("Have a Good Day {}\n Thanks for using our service".format(name))
+    print("")
+    print("Thanks for using our service".format(name))
+    print("")
